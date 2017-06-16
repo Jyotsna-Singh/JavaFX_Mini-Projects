a1a023dddddb3ce2911d3256e881f56a94a4d372
Jyotsna-Singh <jyotsna_singh94@yahoo.co.in>
2017-06-16 15:12:12 +0530

    Image Viewer Pane

---
+++ ImageViewDemo/src/imageviewdemo/ImageViewDemo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imageviewdemo;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ImageViewDemo extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Image pic = new Image("imageviewdemo/goals-vertical.PNG");
+        ImageView ivPic = new ImageView(pic);
+        ivPic.setFitHeight(600);
+        ivPic.setFitWidth(500);
+        
+        BorderPane pane = new BorderPane();
+        pane.setCenter(ivPic);
+        
+        Scene scene = new Scene(pane, 400, 300);
+        
+        primaryStage.setTitle("Image Viewer");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
